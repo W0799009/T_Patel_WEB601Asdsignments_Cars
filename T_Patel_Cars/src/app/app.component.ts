2a4b4237f4e7fe3be5cd816addd99aaf1a20b975
W0799009 <TP94@myscc.ca>
2023-02-10 23:26:55 -0500

    commit all

--- T_Patel_Cars/src/app/app.component.ts
+++ T_Patel_Cars/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Content } from './helper-files/content-interface';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'T_Patel_Cars';
+  contentArray: Content[] = [];
 }
--- T_Patel_Cars/src/app/content-list/content-list.component.ts
+++ T_Patel_Cars/src/app/content-list/content-list.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+//import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+import { ContentList } from '../helper-files/content-list';
+
 
 @Component({
   selector: 'app-content-list',
@@ -7,4 +11,64 @@ import { Component } from '@angular/core';
 })
 export class ContentListComponent {
 
+  contentArray: Content[];
+
+  constructor() {
+    this.contentArray = [
+      {
+        id: 1,
+        title: 'mySampleTitle1',
+        description: 'This is a description1',
+        creator: 'Creator1',
+        imgURL: 'https://media.4rgos.it/i/Argos/9520103_R_Z001A?w=1500&h=880&qlt=70&fmt=webp',
+        type: 'rock music',
+        tags: ['Tag 1.1', 'Tag 1.2'],
+      },
+      {
+        id: 2,
+        title: 'mySampleTitle2',
+        description: 'This is a description2',
+        creator: 'Creator2',
+        imgURL: 'https://media.4rgos.it/i/Argos/9520608_R_Z001A?w=1500&h=880&qlt=70&fmt=webp',
+        type: 'rock music',
+        tags: ['Tag 1.1', 'Tag 1.2'],
+      },
+      {
+        id: 3,
+        title: 'mySampleTitle3',
+        description: 'This is a description3',
+        creator: 'Creator3',
+        imgURL: 'https://media.4rgos.it/i/Argos/9520055_R_Z001A?w=1500&h=880&qlt=70&fmt=webp',
+        type: 'rock music',
+        tags: ['Tag 1.1', 'Tag 1.2'],
+      },
+      {
+        id: 4,
+        title: 'mySampleTitle4',
+        description: 'This is a description4',
+        creator: 'Creator4',
+        imgURL: 'https://media.4rgos.it/i/Argos/9520103_R_Z001A?w=1500&h=880&qlt=70&fmt=webp',
+        type: 'rock music',
+        tags: ['Tag 1.1', 'Tag 1.2'],
+      },
+      {
+        id: 5,
+        title: 'mySampleTitle5',
+        description: 'This is a description5',
+        creator: 'Creator5',
+        imgURL: 'https://media.4rgos.it/i/Argos/9520608_R_Z001A?w=1500&h=880&qlt=70&fmt=webp',
+        type: 'rock music',
+        tags: ['Tag 1.1', 'Tag 1.2'],
+      },
+      {
+        id: 6,
+        title: 'mySampleTitle6',
+        description: 'This is a description6',
+        creator: 'Creator6',
+        imgURL: 'https://media.4rgos.it/i/Argos/9520055_R_Z001A?w=1500&h=880&qlt=70&fmt=webp',
+        type: 'rock music',
+        tags: ['Tag 1.1', 'Tag 1.2'],
+      }
+    ];
+  }
 }
