0c85e1b0f74ed18e18b4b98b00d66f79f3a9b68c
W0799009 <TP94@myscc.ca>
2023-02-21 13:26:48 -0500

    commit css

--- T_Patel_Cars/src/app/hover-affect.directive.ts
+++ T_Patel_Cars/src/app/hover-affect.directive.ts
@@ -1,20 +1,33 @@
-import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHoverAffect]'
 })
 export class HoverAffectDirective {
+  @Input('appHoverAffect') hoverBoldStyle: string | undefined;
 
-  @HostBinding('style.textDecoration') textDecoration: string | undefined;
+  constructor(private el: ElementRef) { }
 
-    @HostListener('mouseenter') onMouseEnter() {
-      this.textDecoration = 'underline';
+  @HostListener('mouseenter') onMouseEnter() {
+    if (this.hoverBoldStyle === 'underline') {
+      this.el.nativeElement.style.textDecoration = 'underline';
     }
-
-    @HostListener('mouseleave') onMouseLeave() {
-      this.textDecoration = 'none';
+    else if (this.hoverBoldStyle === 'bold') {
+      this.el.nativeElement.style.fontWeight = 'bold';
     }
   }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    if (this.hoverBoldStyle === 'underline') {
+      this.el.nativeElement.style.textDecoration = 'none';
+    }
+    else if (this.hoverBoldStyle === 'bold') {
+      this.el.nativeElement.style.fontWeight = 'normal';
+    }
+  }
+
+}
+
 
 
 
